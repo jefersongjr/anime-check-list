80098d72207b42dd7def5a7adb27d45360502601
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-06-04 14:11:51 -0300

    feat :sparkles: criando  tabela de animes

--- frontend/src/pages/AnimeList.jsx
+++ frontend/src/pages/AnimeList.jsx
@@ -1,7 +1,45 @@
+import animes from '../db/animes';
+
 function AnimeList() {
   return (
     <section className="main-section">
-      <p>ola mundo</p>
+      <table>
+        <thead>
+          <tr>
+            <th>Anime</th>
+            <th>Ultimo Episódios</th>
+            <th>Espisódios Assistidos</th>
+            <th>Status</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          { animes && animes.map((anime) => (
+            <tr key={ anime.id }>
+              <td>{anime.animeName}</td>
+              <td>{anime.watchedEpisodes}</td>
+              <td>{anime.lastEpisode}</td>
+              <td>{anime.status}</td>
+              <td>
+                <button
+                  type="button"
+                >
+                  Editar
+                </button>
+              </td>
+              <td>
+                <button
+                  type="button"
+                >
+                  Excluir
+                </button>
+              </td>
+            </tr>
+
+          ))}
+        </tbody>
+      </table>
+
     </section>
   );
 }
