0cb9da11612f1edbc791795d601961a707fa6fee
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-06-04 14:11:29 -0300

    feat :sparkles: criando dados para desenvolvimento

---
+++ frontend/src/db/animes.js
@@ -0,0 +1,28 @@
+const animeList = [
+  {
+    id: 1,
+    animeName: 'One Piece',
+    status: 'Em lançamento',
+    watchedEpisodes: 1105,
+    lastEpisode: 1107,
+    weeklyEspisode: false,
+  },
+  {
+    id: 2,
+    animeName: 'Mashle',
+    status: 'Hiato',
+    watchedEpisodes: 24,
+    lastEpisode: 24,
+    weeklyEspisode: false,
+  },
+  {
+    id: 3,
+    animeName: 'FullMetal Alchemist Brotherhoods',
+    status: 'Completo',
+    watchedEpisodes: 64,
+    lastEpisode: 64,
+    weeklyEspisode: false,
+  },
+];
+
+export default animeList;
