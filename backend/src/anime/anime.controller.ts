1e18a0ead3311e9d0619a5052a649dd8985175de
Jeferson Gomes <jeferson_gjr@hotmail.com>
2024-06-05 00:03:49 -0300

    feat :sparkles: criando metodo post para criar adicionar dados de um novo anime

--- backend/src/anime/anime.controller.ts
+++ backend/src/anime/anime.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { AnimeService } from './anime.service';
 import { Anime } from './anime.entity';
 
@@ -9,5 +9,10 @@ export class AnimeController {
   @Get()
   findAll(): Promise<Anime[]> {
     return this.animeService.findAll();
+  }
+
+  @Post()
+  create(@Body() anime: Anime): Promise<Anime> {
+    return this.animeService.create(anime);
   }
 }
